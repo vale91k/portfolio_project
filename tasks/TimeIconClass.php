b2d9c494a950dc04717066b5276b960512789e5b
Ivan Kovalev <falconchik2517@gmail.com>
2022-12-12 13:43:10 +0300

    4-task Реализация метода по получению иконки

--- tasks/TimeIconClass.php
+++ tasks/TimeIconClass.php
@@ -12,9 +12,20 @@ class TimeIconClass
      * 2. Дописать метод getTimeIcon
      * 3. Через require на главную заимпортить данный класс и вызвать метод в вёрстке для получения ссылки на картинку
      */
-     public static function getTimeIcon()
-     {
-        $url = '';
+    public static function getTimeIcon()
+    {
+
+        $curTime = date("H");
+        if ($curTime > 0 and $curTime < 6) {
+            $url = "../html/img/night.png";
+        } elseif ($curTime >= 6 and $curTime < 12) {
+            $url = "../html/img/morning.png";
+        } elseif ($curTime >= 12 and $curTime < 23) {
+            $url = "../html/img/dayly.png";
+        } else {
+            $url = "../html/img/night.png";
+        }
+
         return $url;
-     }
+    }
 }
