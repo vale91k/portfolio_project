6e86349e15f6d5282cf76cc07f07bf2b83b53c0e
Ivan Kovalev <falconchik2517@gmail.com>
2022-12-17 14:44:47 +0300

    9,10,11,15 - tasks, Перенес наработки в новый шаблон и избавился от ненужных файлов

--- header.php
+++ header.php
@@ -12,7 +12,7 @@ $urlToPicture = new TimeIconClass();
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="styles/css/style.css">
+    <link rel="stylesheet" href="local/templates/15-task/styles/css/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Yanone+Kaffeesatz:wght@200;600&display=swap" rel="stylesheet">
     <title>Ivan's Blog</title>
 </head>
