2642ba3aed9827021cf5127bfb6a23080ae2c242
Ivan Kovalev <falconchik2517@gmail.com>
2023-01-09 01:59:05 +0300

    6-task перевод всех ключей в геттеры и сеттеры

--- 6-task/testClass/Weather.php
+++ 6-task/testClass/Weather.php
@@ -3,14 +3,61 @@
 
 class Weather
 {
+    /**
+     * @var string Ключ для генерации ссылки
+     */
     const API_KEY = '76eaa38077084ff9b6895618221212&q=';
+    /**
+     * @var string Url для генерации ссылки
+     */
     const BASE_URL = 'http://api.weatherapi.com/v1/current.json?key=';
+    /**
+     * @var string Дефолтное название города
+     */
     const DEFAULT_CITY = 'Magnitogorsk';
     /**
      * Название города
      * @var string
      */
     private string $cityName;
+    /**
+     * Название города с приходящего массива с данными
+     * @var string
+     */
+    private string $locationName;
+    /**
+     * Температура цельсия
+     * @var string
+     */
+    private string $tempValue;
+    /**
+     * Влажность погоды
+     * @var string
+     */
+    private string $humidityValue;
+    /**
+     * Скорость ветра
+     * @var string
+     */
+    private string $windValue;
+    /**
+     * Текущее время в городе
+     * @var string
+     */
+    private string $localTime;
+    /**
+     * Иконка погоды
+     * @var string
+     */
+    private string $iconWeather;
+
+    /**
+     * Weather constructor.
+     */
+    public function __construct()
+    {
+        $this->getWeatherData();
+    }
 
     /**
      * Метод возвращает строку, которая несет в себе название города
@@ -36,6 +83,126 @@ class Weather
     }
 
     /**
+     * Возвращаеет строку, навзание города с массива данных
+     * @return string
+     */
+    public function getLocationName(): string
+    {
+        return $this->locationName;
+    }
+
+    /**
+     * Устанавливает название города
+     * @param string $locationName
+     * @return $this
+     */
+    public function setLocationName(string $locationName): Weather
+    {
+        $this->locationName = $locationName;
+        return $this;
+    }
+
+    /**
+     * Возвращает температуру цельсия
+     * @return string
+     */
+    public function getTempValue(): string
+    {
+        return $this->tempValue;
+    }
+
+    /**
+     * Устанавливает температуру с массива данных
+     * @param string $tempValue
+     * @return $this
+     */
+    public function setTempValue(string $tempValue): Weather
+    {
+        $this->tempValue = $tempValue;
+        return $this;
+    }
+
+    /**
+     * Возвращает влажность температуры
+     * @return string
+     */
+    public function getHumidityValue(): string
+    {
+        return $this->humidityValue;
+    }
+
+    /**
+     * Устанавливает влажность температуры
+     * @param string $humidityValue
+     * @return $this
+     */
+    public function setHumidityValue(string $humidityValue): Weather
+    {
+        $this->humidityValue = $humidityValue;
+        return $this;
+    }
+
+    /**
+     * Возвращает скорость ветра
+     * @return string
+     */
+    public function getWindValue(): string
+    {
+        return $this->windValue;
+    }
+
+    /**
+     * Устанавливает скорость ветра
+     * @param string $windValue
+     * @return $this
+     */
+    public function setWindValue(string $windValue): Weather
+    {
+        $this->windValue = $windValue;
+        return $this;
+    }
+
+    /**
+     * Возвращает местное время
+     * @return string
+     */
+    public function getLocalTime(): string
+    {
+        return $this->localTime;
+    }
+
+    /**
+     * Устанавливает местное время
+     * @param string $localTime
+     * @return $this
+     */
+    public function setLocalTime(string $localTime): Weather
+    {
+        $this->localTime = $localTime;
+        return $this;
+    }
+
+    /**
+     * Возвращает иконку погоды
+     * @return string
+     */
+    public function getIconWeather(): string
+    {
+        return $this->iconWeather;
+    }
+
+    /**
+     * Устанавливает иконку погоды
+     * @param string $iconWeather
+     * @return $this
+     */
+    public function setIconWeather(string $iconWeather): Weather
+    {
+        $this->iconWeather = $iconWeather;
+        return $this;
+    }
+
+    /**
      * Метод генерирует ссылку, по которой получим данные о погоде.
      * @return string
      */
@@ -55,35 +222,34 @@ class Weather
 
     /**
      * Метод получает отфильтрованный массив с данными о погоде
-     * @return array
+     * @return Weather
      */
-    public function getWeatherData(): array
+    public function getWeatherData(): Weather
     {
         $weatherData = $this->requestWeatherData();
-        return [
-            'location_name' => $weatherData["location"]["name"],
-            'temp_c' => $weatherData["current"]["temp_c"],
-            'humidity' => $weatherData["current"]["humidity"],
-            'wind' => $weatherData["current"]["wind_kph"],
-            'localTime' => $weatherData["location"]["localtime"],
-            'icon' => $weatherData["current"]["condition"]["icon"],
-        ];
+        $this->setLocationName($weatherData["location"]["name"]);
+        $this->setTempValue($weatherData["current"]["temp_c"]);
+        $this->setHumidityValue($weatherData["current"]["humidity"]);
+        $this->setWindValue($weatherData["current"]["wind_kph"]);
+        $this->setLocalTime($weatherData["location"]["localtime"]);
+        $this->setIconWeather($weatherData["current"]["condition"]["icon"]);
+        return $this;
+
     }
 }
 
 $weather = new Weather();
-$weatherResult = $weather->getWeatherData();
-
 echo '<pre>';
-print_r($weatherResult);
+var_dump($weather);
 echo '</pre>';
 
 
 ?>
 <div class="weather">
-    <h2>Погода в городе <?= $weatherResult["location_name"]; ?></h2>
-    <p>Погода: <?= $weatherResult["temp_c"]; ?>°C</p>
-    <p>Влажность: <?= $weatherResult["humidity"]; ?> %</p>
-    <p>Ветер: <?= $weatherResult["wind"]; ?> км/ч</p>
-    <p>Дата/Время: <?= $weatherResult["localTime"]; ?> </p>
+    <h2>Погода в городе <?= $weather->getLocationName(); ?></h2>
+    <p>Погода: <?= $weather->getTempValue(); ?>°C</p>
+    <p>Влажность: <?= $weather->getHumidityValue() ?> %</p>
+    <p>Ветер: <?= $weather->getWindValue() ?> км/ч</p>
+    <p>Дата/Время: <?= $weather->getLocalTime() ?> </p>
+    <img class="header__picture" src="<?= $weather->getIconWeather(); ?>">
 </div>
