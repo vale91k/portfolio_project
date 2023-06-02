af6ae9797910980f6a5e3817d8b736ac1d378683
Kovalev.v <kovalev_vs@rosakhutor.com>
2023-06-02 17:39:14 +0500

    Реализация пространства модели по битриксу

--- index.php
+++ index.php
@@ -2,5 +2,11 @@
 
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Ivan's Blog");
+/*
+ Пример использования модели и поиска по ИД
+ use App\Models\Games\Game;
 
+$game = Game::fetchById(1);
+var_dump($game);
+*/
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
