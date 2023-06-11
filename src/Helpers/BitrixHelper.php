7f208fd6bbf71b637c38b5cec75298854bcff080
Ivan Kovalev <falconchik2517@gmail.com>
2023-06-11 21:26:31 +0300

    49-task Исправление замечаний

--- src/Helpers/BitrixHelper.php
+++ src/Helpers/BitrixHelper.php
@@ -12,28 +12,32 @@ class BitrixHelper
 {
     /**
      * Возвращает Id инфоблока
-     * @param $code
+     * @param string $code
      * @return string
      */
-    static function getIdIBlockByCode($code): string
+    static function getIdIBlockByCode(string $code): string
     {
         $arrFilter = [
             'ACTIVE' => 'Y',
             'CODE' => $code,
-            'SITE_ID' => "s1",
+            'SITE_ID' => SITE_ID,
         ];
-        $result = \CIBlock::GetList(["SORT" => "ASC"], $arrFilter, false);
+        $result = \CIBlock::GetList(false, $arrFilter, false);
         $iBlockId = $result->Fetch();
-        return $iBlockId["ID"];
+        if (isset ($iBlockId["ID"])) {
+            return $iBlockId["ID"];
+        }
+        return '';
+
     }
 
     /**
      * Возвращает id раздела
-     * @param $code
-     * @param $iBlockId
+     * @param string $code
+     * @param string $iBlockId
      * @return string
      */
-    static function getIdSectionByCode($code, $iBlockId): string
+    static function getIdSectionByCode(string $code, string $iBlockId): string
     {
         $result = \CIBlockSection::GetList([],
             [
@@ -42,26 +46,39 @@ class BitrixHelper
             ]
         );
         $sectionId = $result->Fetch();
-        return $sectionId['ID'];
+        if (isset ($sectionId['ID'])) {
+            return $sectionId['ID'];
+        }
+        return '';
     }
 
     /**
      * Возвращает id элемента
-     * @param $code
-     * @param $iBlockId
-     * @param $sectionId
+     * @param string $code
+     * @param string $iBlockId
+     * @param string|null $sectionId
      * @return string
      */
-    static function getIdElementByCode($code, $iBlockId, $sectionId): string
+    static function getIdElementByCode(string $code, string $iBlockId, string $sectionId = null): string
     {
-        $arFilter = [
-            "IBLOCK_ID" => $iBlockId,
-            "IBLOCK_SECTION_ID" => $sectionId,
-            "CODE" => $code
-        ];
+        if (!is_null($sectionId)) {
+            $arFilter = [
+                "IBLOCK_ID" => $iBlockId,
+                "IBLOCK_SECTION_ID" => $sectionId,
+                "CODE" => $code
+            ];
+        } else {
+            $arFilter = [
+                "IBLOCK_ID" => $iBlockId,
+                "CODE" => $code
+            ];
+        }
         $result = \CIBlockElement::GetList([], $arFilter, false, ["nPageSize" => 1], ['ID']);
         $elementId = $result->Fetch();
-        return $elementId['ID'];
+        if (isset ($elementId['ID'])) {
+            return $elementId['ID'];
+        }
+        return '';
     }
 
 
