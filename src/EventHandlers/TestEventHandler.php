079b7ab8063579df02fde851b8f1c6b9b22eecac
Ivan Kovalev <falconchik2517@gmail.com>
2023-07-02 23:45:09 +0300

    ex2-51 Исправления замечаний

--- src/EventHandlers/TestEventHandler.php
+++ src/EventHandlers/TestEventHandler.php
@@ -4,7 +4,39 @@ namespace App\EventHandlers;
 
 class TestEventHandler
 {
-    public function test() {
-        // TODO переименовать класс и сделать реализацию по Ex2_51
+    /**
+     * Запись в журнал событий при отправке писем с компонента bitrix:main.feedback
+     * @param string $event Идентификатор почтового события
+     * @param string $lid Id сайта
+     * @param array $arFields Массив параметров
+     */
+    public function feedbackService(string &$event, string &$lid, array &$arFields): void
+    {
+        if ($event == "FEEDBACK_FORM") {
+            global $USER;
+            // Если авторизован
+            if ($USER->IsAuthorized()) {
+                $arFields["AUTHOR"] = GetMessage("EX2_51_AUTH", [
+                    '#ID#' => $USER->GetID(),
+                    '#LOGIN#' => $USER->GetLogin(),
+                    '#NAME#' => $USER->GetFirstName(),
+                    '#NAME_FORM#' => $arFields["AUTHOR"]
+                ]);
+            } else {
+                // Если не авторизован
+                $arFields["AUTHOR"] = GetMessage("EX2_51_NOT_AUTH", [
+                    '#NAME_FORM#' => $arFields["AUTHOR"]
+                ]);
+            }
+            // Запись в журнал событий
+            \CEventLog::Add([
+                "SEVERITY" => "SECURITY",
+                "AUDIT_TYPE_ID" => "LETTER_CHANGED",
+                "MODULE_ID" => "main",
+                "DESCRIPTION" => GetMessage("EX2_51_INFO_CHANGED", [
+                    '#NAME_FORM#' => $arFields["AUTHOR"]
+                ]),
+            ]);
+        }
     }
 }
