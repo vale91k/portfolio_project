f618d0e36efb62f2a44e4acf7af7f421f6d067c3
Ivan Kovalev <falconchik2517@gmail.com>
2023-08-06 14:14:08 +0300

    ex2-104 Исправление страницы

--- index.php
+++ index.php
@@ -7,21 +7,6 @@ $APPLICATION->SetTitle("Ivan's Blog");
         <div class="container">
             <div class="main_page__inner">
                 <h1 class="main_page__title">Welcome to my page</h1>
-                <?php
-                $arr = [1,2,3,4,4,4,5,6,7,8,9,9,9];
-
-                // Считаем количество значений
-                $count_values = array_count_values($arr);
-
-//                print_r($count_values);
-
-                // Фильтруем те которых больше 1
-                $repits = array_filter($count_values, fn($el) => $el == 1);
-
-                echo '<pre>';
-                print_r($repits);
-                echo '</pre>';
-                ?>
             </div>
         </div>
     </div>
