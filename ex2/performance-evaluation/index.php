6d3b40712e6964e2b2d097d8b49aded3e71657d2
Ivan Kovalev <falconchik2517@gmail.com>
2023-09-05 18:55:47 +0300

    ex2-88 Задания по выявлению самой загруженной страницы, компонента, запросов от компонента

---
+++ ex2/performance-evaluation/index.php
@@ -0,0 +1,18 @@
+<?php
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Оценка производительности");
+?>
+Самая ресурсоемкая страница: /products/index.php. Доля нагрузки: 19.13%
+</br>
+Работа компонента по умолчанию: 15 КБ
+</br>
+Работа компонента, только нужные параметры: кеш: 7 КБ
+</br>
+Разница 15 КБ - 7 КБ = 8 КБ
+</br>
+Самый загруженный компонент: bitrix:catalog: 0.0376 с
+</br>
+Компонент с наибольшим количеством запросов: bitrix:catalog.section. Запросов 19.
+
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
